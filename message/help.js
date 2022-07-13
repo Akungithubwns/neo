ed0c01f3d7a48a8c6c59e734b2cada3f11b8007e
Andynurohman <andinurrohman577@gmail.com>
2022-07-13 17:51:31 +0700

    neo

---
+++ message/help.js
@@ -0,0 +1,18 @@
+exports.txtSewabot = (prefix) => {
+return `
+ 1-Minggu : Rp1.000
+ 2-Minggu : Rp.2.000
+ 1-Bulan : Rp.5k.000
+ 2-Bulan : Rp.7k.000
+ 3-Bulan : Rp10k.000
+`
+}
+exports.txtDonate = (prefix) => {
+return `
+▷_*DONASI BOT*_
+ _- DANA : 082138899290_
+ _- OVO : 082138899290_
+ _- GOPAY : 082138899290_
+ _- QRIS : -
+`
+}
